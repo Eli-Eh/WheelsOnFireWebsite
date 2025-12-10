001886cce570fefebbe5e318319af80fe5e855c9
eh <118851818+Eli-Eh@users.noreply.github.com>
2025-12-10 00:10:14 +0000

    Update contactValidation.js
    added comments B) also removed unnecessary variable

--- scripts/contactValidation.js
+++ scripts/contactValidation.js
@@ -1,14 +1,22 @@
+/*
+	Description: This program validates the forms found in contact.html beyond the
+	standard html validation found in html forms. Also handles redirecting the
+	user to the homepage once they press submit. (in a very bodge way)
+
+	Author: Illia Sergeyev
+*/
+
 // returns true if the entered string is ONLY alphabetic
 function isAlpha(stringToTest){
 	return /^[A-Za-z]+$/.test(stringToTest);
 }
 
+// main function used to validate the titular form
 function validate()
 {
     let firstName = document.forms["contactform"]["firstname"].value;
     let lastName = document.forms["contactform"]["lastname"].value;
 	let reason = document.forms["contactform"]["reason"].value;
-	let isAlphabetic = /^[A-Za-z]+$/.test();
 
 	// the first and last names MUST be at least 2 characters long
     if(firstName.length < 2 || lastName.length < 2){
@@ -16,6 +24,7 @@ function validate()
 		return false;
     }
 
+	// the first and last names MUST also have only alpabetic characters
     else if (!isAlpha(firstName) || !isAlpha(lastName)){
 		alert("Your name may only include alphabetic characters.");
 		return false;
@@ -32,7 +41,7 @@ function validate()
 		}
 		// redirect to home page
 		document.location = '../index.html';
-		// alert is here because there is once again, a ghost in the code D:
+		// alert is here because there is once again, a vengeful spirit in the code D:
 		// i think it shows up on chromium browsers, and gets interupted in firefox based browsers
 		alert("Now sending you to home page.");
 		return true;
